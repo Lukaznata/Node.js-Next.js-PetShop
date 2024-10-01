0ab14097af996db8283cc9b0bbd783bfb1df18c9
Lucas Nathan Tonin <lucas18tonin@gmail.com>
2024-10-01 00:54:42 -0300

    .

--- src/db/conn.ts
+++ src/db/conn.ts
@@ -3,15 +3,32 @@ dotenv.config();
 
 import { Sequelize } from "sequelize";
 
+// const sequelize = new Sequelize(
+//   "teste2_petshop",
+//   "root",
+//   process.env.PASSWORD_MYSQL,
+//   {
+//     // host: process.env.HOST,
+//     host: process.env.DATABASE_URL,
+//     dialect: "mysql",
+//   }
+// );
+
+const port = Number(process.env.MYSQL_PORT) || 3306;
+
 const sequelize = new Sequelize(
-  "teste2_petshop",
+  "petshopDB",
   "root",
-  process.env.PASSWORD_MYSQL,
+  process.env.MYSQL_PASSWORD,
   {
-    // host: process.env.HOST,
-    host: process.env.DATABASE_URL,
+    host: process.env.HOST,
+    port,
     dialect: "mysql",
   }
 );
+
+
+
+
 
 export default sequelize;
