1728ed80022cee61c09cfd5583c41736e2b9441b
Lucas Nathan Tonin <lucas18tonin@gmail.com>
2024-10-03 09:53:55 -0300

    .

--- src/db/conn.ts
+++ src/db/conn.ts
@@ -14,21 +14,30 @@ import { Sequelize } from "sequelize";
 //   }
 // );
 
-const port = Number(process.env.MYSQLPORT)
-
-const sequelize = new Sequelize(
-  process.env.MYSQLDATABASE || 'petshop',
-  process.env.MYSQLUSER || 'root',
-  process.env.MYSQLPASSWORD,
-  {
-    dialect: "mysql",
-    host: process.env.MYSQLHOST,
-    port: port,
-  }
-);
 
 
+// const port = parseInt(process.env.MYSQLPORT, 10);
 
+// const sequelize = new Sequelize(
+//   process.env.MYSQLDATABASE || 'petshop',
+//   process.env.MYSQLUSER || 'root',
+//   process.env.MYSQLPASSWORD,
+//   {
+//     dialect: "mysql",
+//     host: process.env.MYSQLHOST,
+//     port: port,
+//   }
+// );
+
+
+
+if (!process.env.MYSQL_URL) {
+  throw new Error('MYSQL_URL is not defined in the environment variables');
+}
+
+const sequelize = new Sequelize(process.env.MYSQL_URL, {
+  dialect: "mysql",
+});
 
 
 export default sequelize;
