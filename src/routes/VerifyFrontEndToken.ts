e7016cbbfefacf48dd7c8dfba37c6ee4dd5604f2
Lucas Nathan Tonin <lucas18tonin@gmail.com>
2024-10-11 16:12:09 -0300

    .

---
+++ src/routes/VerifyFrontEndToken.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import VerifyToken from "../helpers/verifyToken";
+
+const router = express.Router();
+
+router.get("/auth/verify", VerifyToken, (req, res) => {
+  return res.status(200).json({ message: "Token válido" });
+});
