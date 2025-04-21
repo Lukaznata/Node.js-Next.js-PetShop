0e4f564850a952b2dcf2640d5e334a2cd6fa74d4
Lucas Nathan <lucas18tonin@gmail.com>
2025-04-20 21:18:15 -0300

    .

--- backend/wait-for.sh
+++ backend/wait-for.sh
@@ -6,9 +6,12 @@ PORT=$2
 shift 2
 CMD="$@"
 
-until nc -z "$HOST" "$PORT"; do
+echo "Esperando por $HOST:$PORT..."
+
+while ! echo > /dev/tcp/$HOST/$PORT 2>/dev/null; do
   echo "Aguardando $HOST:$PORT..."
   sleep 2
 done
 
+echo "$HOST:$PORT está disponível. Iniciando comando..."
 exec $CMD
